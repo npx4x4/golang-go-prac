fdf753ed3002c6556291c8e26d8be0706b85e87d
npx4x4 <mickgame1881@gmail.com>
2026-04-24 13:37:29 +0900

    ソルバ用変数の登録まで

--- Solver/ranking.go
+++ Solver/ranking.go
@@ -13,12 +13,24 @@ func SolveRanking(Members[],  ) {
 	config.Close()
 	defer ctx.Close()
 
-	//Solverを初期化
+	// Solverを初期化
 	s := ctx.NewSolver()
 	defer s.Close()
 
-	z3Members := make{map[string]}
+	z3Members := make{map[string]z3.Int}
 	// 各メンバーを変数として登録
-	for _, Member := range Members:
-		Members[]
+	for _, Member := range Members {
+		z3Members[Member] = ctx.Const(ctx.Symbol(Member), ctx.IntSort())
+		s.Assert(z3Members[Member].Gt(zero))
+		s.Assert(z3Members(Member).Lt(RankingSize+1))
+	}
+
+	for _, Uwasa := range UwasaBox {
+
+	}
+
+	if v := s.Check(); v != True {
+		fmt.println("いけてます")
+		return
+	}
 }
