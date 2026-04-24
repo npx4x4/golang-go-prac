1cdba64370bc511af3ef05480e43e21753134d0e
npx4x4 <mickgame1881@gmail.com>
2026-04-24 09:29:05 +0900

    比較(Relation)専用の型を追加

--- model/types.go
+++ model/types.go
@@ -1,7 +1,16 @@
 package model
 
+type Relation string
+
+const (
+	OrHigher Relation = ">="
+	Equal    Relation = "=="
+	OrUnder  Relation = "<="
+	NoEqual  Relation = "!="
+)
+
 type Uwasa struct {
 	Name     string
 	Target   string
-	Relation string
+	Relation Relation
 }
